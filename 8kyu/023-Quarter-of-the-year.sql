0a83e72d61b8c94264501e392c2757228031f33b
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-06 11:30:38 +0700

    Create 8kyu\023-Quarter-of-the-year.sql

---
+++ 8kyu/023-Quarter-of-the-year.sql
@@ -0,0 +1,7 @@
+-- # write your SQL statement here: you are given a table 'quarterof' with column 'month', return a table with column 'month' and your result in a column named 'res'.
+SELECT month, 
+CASE
+  WHEN month%3 > 0 THEN month/3+1
+  ELSE month/3
+END
+AS res FROM quarterof;
