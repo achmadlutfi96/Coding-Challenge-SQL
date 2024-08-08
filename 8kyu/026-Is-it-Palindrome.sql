cd0a3d8dfe5e92eb8a7e646bd68a024f317d0460
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-08 12:02:32 +0700

    Create 8kyu\026-Is-it-Palindrome.sql

---
+++ 8kyu/026-Is-it-Palindrome.sql
@@ -0,0 +1,2 @@
+-- # write your SQL statement here: you are given a table 'ispalindrome' with column 'str', return a table with column 'str' and your result in a column named 'res'.
+SELECT str, LOWER(REVERSE(str)) = LOWER(str) AS res FROM ispalindrome;
