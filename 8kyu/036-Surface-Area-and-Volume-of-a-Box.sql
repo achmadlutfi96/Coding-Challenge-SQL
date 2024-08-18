5048d44c785d0461cb0ade3f9e187fc12f72d07a
Achmad Lutfi <alhacker1996@gmail.com>
2024-08-18 12:33:03 +0700

    Create 8kyu\036-Surface-Area-and-Volume-of-a-Box.sql

---
+++ 8kyu/036-Surface-Area-and-Volume-of-a-Box.sql
@@ -0,0 +1,9 @@
+-- # write your SQL statement here: 
+-- you are given a table 'box' with columns: width (int), height (int), depth (int)
+-- return a query with columns: width, height, depth, area (int), volume (int)
+-- sort results by area ascending, then volume ascending, then width ascending, then height ascending
+SELECT width, height, depth, 
+2*((width*height)+(height*depth)+(depth*width)) AS area, 
+width*height*depth AS volume 
+FROM box 
+ORDER BY area , volume, width, height;
