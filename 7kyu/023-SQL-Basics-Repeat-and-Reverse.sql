d9ef33b6e4b581982c99798e3a850e3e0427746f
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-10 12:58:55 +0700

    Update 7kyu\023-SQL-Basics-Repeat-and-Reverse.sql

--- 7kyu/023-SQL-Basics-Repeat-and-Reverse.sql
+++ 7kyu/023-SQL-Basics-Repeat-and-Reverse.sql
@@ -1,2 +1,5 @@
 /*  SQL  */
 SELECT CONCAT(name,name,name) AS name, REVERSE(characteristics) AS characteristics FROM monsters;
+
+-- Alternative
+SELECT REPEAT(name, 3) AS name, REVERSE(characteristics) AS characteristics FROM monsters
