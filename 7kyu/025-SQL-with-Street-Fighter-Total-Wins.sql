49cf3cac577ffefa15d206a23703c1f8e086a40e
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-09-11 13:34:53 +0700

    Create 7kyu\025-SQL-with-Street-Fighter-Total-Wins.sql

---
+++ 7kyu/025-SQL-with-Street-Fighter-Total-Wins.sql
@@ -0,0 +1,9 @@
+--- 3, 2, 1, fight! ---
+SELECT f.name, SUM(f.won) as won, SUM(f.lost) as lost 
+FROM fighters as f
+LEFT JOIN winning_moves as w
+ON f.move_id = w.id
+WHERE w.move NOT IN ('Hadoken', 'Shouoken', 'Kikoken')
+GROUP BY f.name 
+ORDER BY won DESC
+LIMIT 6;
